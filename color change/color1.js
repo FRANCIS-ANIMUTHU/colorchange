602ef8ed1f678ab6b20a6d64447feeb4e6c28a97
FRANCIS-ANIMUTHU <104548375+FRANCIS-ANIMUTHU@users.noreply.github.com>
2022-07-09 21:44:55 +0530

    Add files via upload

---
+++ color change/color1.js
@@ -0,0 +1,77 @@
+
+input.addEventListener('mouseout', colors);
+function colors() {
+     var input = document.getElementById('input');
+     var text = input.value;
+     input.style.color = text;
+}
+
+input2.addEventListener('mouseout', colors2);
+function colors2() {
+     var input2 = document.getElementById('input2');
+     var text2 = input2.value;
+     input2.style.color = text2;
+}
+
+input3.addEventListener('mouseout', colors3);
+function colors3() {
+     var input3 = document.getElementById('input3');
+     var text3 = input3.value;
+     input3.style.color = text3;
+}
+
+input4.addEventListener('mouseout', colors4);
+function colors4() {
+     var input4 = document.getElementById('input4');
+     var text4 = input4.value;
+     input4.style.color = text4;
+}
+
+input5.addEventListener('mouseout', colors5);
+function colors5() {
+     var input5 = document.getElementById('input5');
+     var text5 = input5.value;
+     input5.style.color = text5;
+}
+
+input6.addEventListener('mouseout', colors6);
+function colors6() {
+     var input6 = document.getElementById('input6');
+     var text6 = input6.value;
+     input6.style.color = text6;
+}
+
+input7.addEventListener('mouseout', colors7);
+function colors7() {
+     var input7 = document.getElementById('input7');
+     var text7 = input7.value;
+     input7.style.color = text7;
+}
+
+input8.addEventListener('mouseout', colors8);
+function colors8() {
+     var input8 = document.getElementById('input8');
+     var text8 = input8.value;
+     input8.style.color = text8;
+}
+
+input9.addEventListener('mouseout', colors9);
+function colors9() {
+     var input9 = document.getElementById('input9');
+     var text9 = input9.value;
+     input9.style.color = text9;
+}
+
+input10.addEventListener('mouseout', colors10);
+function colors10() {
+     var input10 = document.getElementById('input10');
+     var text10 = input10.value;
+     input10.style.color = text10;
+}
+
+
+
+
+
+
+
